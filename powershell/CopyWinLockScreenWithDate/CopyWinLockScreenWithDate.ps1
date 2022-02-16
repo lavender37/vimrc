2c497d45f7ef46964b543e1060701c87e144a6fd
lavender37 <19884320+lavender37@users.noreply.github.com>
2022-02-16 13:18:11 +0800

    Update CopyWinLockScreenWithDate.ps1
    修复部分情况下找不到图片的错误
    修复部分情况下误将文件夹识别当作文件进行操作的错误
    修复当启用14行，注释15行时出现的列出文件错误

--- powershell/CopyWinLockScreenWithDate/CopyWinLockScreenWithDate.ps1
+++ powershell/CopyWinLockScreenWithDate/CopyWinLockScreenWithDate.ps1
@@ -4,17 +4,17 @@ $desExtension = ".jpg"
 $currentPath = $MyInvocation.MyCommand.Definition | Split-Path -Parent
 # 获取备份日期
 $dataFormat = Get-Date -format 'yyyyMMdd'
-# 构建目标路径
+# 构建目标路径，这里可以修改目标保存路径
 # $destinationPath = "$currentPath\$dataFormat"
 $destinationPath = "D:\Pictures\$dataFormat"
 if (-not $(Test-Path $destinationPath)) {
     New-Item -Path $destinationPath -ItemType Directory 
 }
 # 配置锁屏图片位置
-# $WindowsLockScreenPath = "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_*\LocalState\Assets"
+# $WindowsLockScreenPath = Get-ChildItem -Path "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_*\LocalState\Assets"
 $WindowsLockScreenPath = "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
 # 列出锁屏位置所有图片
-$listfile = Get-ChildItem -Path $WindowsLockScreenPath
+$listfile = Get-ChildItem -Path $WindowsLockScreenPath | Where-Object { $_.PsIsContainer -eq $false }
 if (-not $listfile) {
     write-host "File did not exit in $listfile[$i]"
 }
@@ -25,7 +25,7 @@ for ($i = 0; $i -lt $listfile.Count; $i++) {
     $desfile = "$destinationPath\$desName"
     if (-not $(Test-Path $desfile)) {
         Copy-Item -Path $srcfile -Destination $desfile 
-        write-host "Successfully moved $listfile[$i] to $desfile"
+        write-host "Successfully copied $listfile[$i] to $desfile"
     }
     else {
         write-host "File $desfile existed and skipped"
@@ -34,3 +34,6 @@ for ($i = 0; $i -lt $listfile.Count; $i++) {
         write-host "Successfully moved $listfile[$i] to $desfile"
     }
 }
+# 任意键退出
+[System.Console]::ReadKey() | Out-Null;
+Exit-PSSession;
